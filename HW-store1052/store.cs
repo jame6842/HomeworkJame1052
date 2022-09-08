e249bd09a7e43930316c909ca6f90d6b44baedfb
jame6842 <112143875+jame6842@users.noreply.github.com>
2022-09-08 22:22:29 +0700

    Add files via upload

---
+++ HW-store1052/store.cs
@@ -0,0 +1,13 @@
+public class store{
+   public string storename;
+   public string shopmanager;
+   public string number;
+   public int cost;
+   
+   public store(string storename,string shopmanager,string number,int cost){
+        this.storename = storename;
+        this.shopmanager = shopmanager;
+        this.number = number;
+        this.cost = cost;
+   }
+}
